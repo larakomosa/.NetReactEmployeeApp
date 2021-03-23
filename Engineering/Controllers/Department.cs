2648462fc3a8c683cbb643708c6fc3eba06332a8
Lara Komosa <lara.komosa@artisantechgroup.com>
2021-03-23 15:20:14 -0500

    Get retrieved from Database

---
+++ Engineering/Controllers/Department.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Engineering.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+
+            }
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ Engineering/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Engineering.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
+
