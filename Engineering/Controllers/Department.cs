6a3999a285529ae8c301919d1af5fd41bbb7e639
Lara Komosa <lara.komosa@artisantechgroup.com>
2021-03-23 15:50:17 -0500

    Post, Update and Delete Routes created and tested

--- Engineering/Controllers/Department.cs
+++ Engineering/Controllers/Department.cs
@@ -6,7 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
 using System.Data;
-
+using Engineering.Models;
+    
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Engineering.Controllers
@@ -48,5 +49,88 @@ namespace Engineering.Controllers
             }
             return new JsonResult(table);
         }
+
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+               insert into dbo.Department values
+                ('"+dep.DepartmentName+@"')";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+
+            }
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            string query = @"
+               update dbo.Department set
+               DepartmentName = '" + dep.DepartmentName + @"'
+               where DepartmentId = '" + dep.DepartmentId + @"'
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+               delete from dbo.Department 
+               where DepartmentId = " + id + @"
+                ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeDB");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+
+
     }
 }
